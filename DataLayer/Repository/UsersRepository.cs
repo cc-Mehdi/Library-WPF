efc4317dde82d73324a2210fc5eec12048cc1ac8
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 19:53:53 +0330

    Implement UsersRepository

---
+++ DataLayer/Repository/UsersRepository.cs
@@ -0,0 +1,24 @@
+﻿using Datalayer.Repository.IRepository;
+using System.Linq;
+
+namespace Datalayer.Repository
+{
+    public class UsersRepository : Repository<Users>, IUsersRepository
+    {
+        private readonly Library_DbEntities _db;
+        public UsersRepository(Library_DbEntities db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Users user)
+        {
+            var objFromDb = _db.Users.FirstOrDefault(u => u.Id == user.Id);
+            objFromDb.UserName = user.UserName;
+            objFromDb.Email = user.Email;
+            objFromDb.Password = user.Password;
+            objFromDb.isSpecial = user.isSpecial;
+            objFromDb.Image = user.Image;
+        }
+    }
+}
