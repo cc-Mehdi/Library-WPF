543deb5ad93a2c83ec21ff100d9ce26299d05144
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 20:11:47 +0330

    Implement Unit Of Work class

---
+++ DataLayer/Repository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Datalayer.Repository.IRepository;
+
+namespace Datalayer.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Library_DbEntities _db;
+        public UnitOfWork(Library_DbEntities db)
+        {
+            _db = db;
+            User = new UsersRepository(_db);
+            Book = new BooksRepository(_db);
+            BorrowedBook = new BorrowedBooksRepository(_db);
+        }
+
+        public IUsersRepository User { get; private set; }
+        public IBooksRepository Book { get; private set; }
+        public IBorrowedBooksRepository BorrowedBook { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
