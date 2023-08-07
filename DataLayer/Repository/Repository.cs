644e3a0941dcfae6836f6b6bc4b85fec70e5a8b9
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 19:46:24 +0330

    Implement Repository class

---
+++ DataLayer/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Datalayer.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalayer.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly Library_DbEntities _db;
+        internal DbSet<T> _dbSet;
+        public Repository(Library_DbEntities db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public IEnumerable<T> GetAllByFilter(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
