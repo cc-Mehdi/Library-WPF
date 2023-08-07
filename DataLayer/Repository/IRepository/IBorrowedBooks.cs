a3bfa43624e12ba92f15549e7e67cca00c08ffee
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 20:02:13 +0330

    Add Borrowed Books interface

---
+++ DataLayer/Repository/IRepository/IBorrowedBooks.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Datalayer.Repository.IRepository
+{
+    public interface IBorrowedBooksRepository : IRepository<BorrowedBooks>
+    {
+        void Update(BorrowedBooks borrowedBooks);
+    }
+}
