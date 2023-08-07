a5230dce22e30c26aa97f0bbae5202936881926b
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 20:05:04 +0330

    Implement Borrowed Books class

---
+++ DataLayer/Repository/BorrowedBooksRepository.cs
@@ -0,0 +1,23 @@
+﻿using Datalayer.Repository.IRepository;
+using System.Linq;
+
+namespace Datalayer.Repository
+{
+    public class BorrowedBooksRepository : Repository<BorrowedBooks>, IBorrowedBooksRepository
+    {
+        private readonly Library_DbEntities _db;
+        public BorrowedBooksRepository(Library_DbEntities db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(BorrowedBooks borrowedBooks)
+        {
+            var objFromDb = _db.BorrowedBooks.FirstOrDefault(u => u.Id == borrowedBooks.Id);
+            objFromDb.BookId = borrowedBooks.BookId;
+            objFromDb.UserId = borrowedBooks.UserId;
+            objFromDb.StartDate = borrowedBooks.StartDate;
+            objFromDb.EndDate = borrowedBooks.EndDate;
+        }
+    }
+}
