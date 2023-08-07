effac498fccaed4a33f890232a5b44a0fdacd964
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 19:42:15 +0330

    Add IRepository Interface

---
+++ DataLayer/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Datalayer.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAllByFilter(Expression<Func<T, bool>> filter = null);
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
