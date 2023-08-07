2a86ae21986132e36718b04727e7c268868333eb
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-07 20:08:42 +0330

    Add Unit Of Work interface

---
+++ DataLayer/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Datalayer.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUsersRepository User { get; }
+        IBooksRepository Book { get; }
+        IBorrowedBooksRepository BorrowedBook { get; }
+        void Save();
+    }
+}
