b6f53b9456f6f2440b4605f4d26f386e77385cfb
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-08 19:57:03 +0330

    Add Filter method to book interface

--- DataLayer/Repository/IRepository/IBooksRepository.cs
+++ DataLayer/Repository/IRepository/IBooksRepository.cs
@@ -1,8 +1,12 @@
 ﻿
+using System;
+using System.Linq.Expressions;
+
 namespace Datalayer.Repository.IRepository
 {
     public interface IBooksRepository : IRepository<Books>
     {
         void Update(Books book);
+        void Filter(Expression<Func<Books, bool>> filter = null);
     }
 }
